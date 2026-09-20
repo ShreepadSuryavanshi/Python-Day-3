f51d47f14f39f7a78990916b8537cc7b5010e101
ShreepadSuryavanshi <shreepadsuryavanshi5@gmail.com>
2026-09-21 00:42:19 +0530

    DAY 4

---
+++ while.py
@@ -0,0 +1,26 @@
+while True:
+     string = input("Enter a string:")
+     print(string)
+
+# correct_pass = "some_pass"
+# not_found = True
+
+# while not_found:
+#     passw = input("Enter pass:")
+#     if passw == correct_pass:
+#         not_found = False
+
+# print("Password matched")  
+
+# i = 1
+# while i<11:
+#     print(i*7)
+#     i+=1
+
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         i +=1
+#         continue
+#     print(i)
+#     i +=1
